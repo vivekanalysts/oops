2e69cc05aec1c4f209c3b42f269fc5cf5e4a0d76
vivekanalysts <vivekkushwaha285@gmail.com>
2026-06-30 07:32:23 +0530

    new

--- oopsgit.py
+++ oopsgit.py
@@ -281,13 +281,202 @@ t.sub()
 
 #========================================
 
+class Product:
+    
+    def prod(self):
+        self.__productid = int(input("Enter ID: "))
+        self.__catagory = input("Enter Category: ")
+        self.__productname = input("Enter Product Name: ")
+        self.__productrate = int(input("Enter Rate: "))
+
+    def showproduct(self):
+        print("Product ID:", self.__productid)
+        print("Category:", self.__catagory)
+        print("Product Name:", self.__productname)
+        print("Rate:", self.__productrate)
+
+    def searchid(self, id):
+        if self.__productid == id:
+            self.showproduct()
+            return True
+        return False
+
+    def searchcat(self, cat):
+        if self.__catagory.lower() == cat.lower():
+            self.showproduct()
+            return True
+        return False
+
+    def searchpat(self, pat):
+        if pat.lower() in self.__productname.lower():
+            self.showproduct()
+            return True
+        return False
 
 
+# Main Program
+i = int(input("Enter how many products:? "))
+b = []
+
+for i in range(i):
+    p = Product()
+    p.prod()
+    b.append(p)
+
+#search by id
+
+id = int(input("Search ID: "))
+
+found = False
+
+for i in b:
+    found = i.searchid(id)
+    if found:break
         
 
+if not found:
+    print("Product doesn't exist...")
 
 
+#search by catargory
+#cat=input("search by catargory:")
+#for i in b:
+#   i.searchcat(cat)
 
+# search by pattern
 
+# pat=input("enter pattern:")
+# for i in b :
+#     i.searchpat(pat)
 
         
+#========================================
+
+class Bank:
+    def openAccount(self):
+        self.__acno=input("Enter Account Number:")
+        self.__name=input("Enter name:")
+        self.__balance=int(input("Enter balance:"))
+        return self.__acno
+
+    def showAccount(self):
+        print(self.__acno,self.__name,self.__balance) 
+         
+    def Deposit(self):
+        amt=int(input("Enter Amount u want to Deposit:"))
+        self.__balance+=amt
+
+    def withdrawal(self):
+        amt=int(input("Enter Amount u want to withdraw:"))
+        if(self.__balance>=amt):
+           self.__balance-=amt
+
+        else:
+            print("Insufficiant Balance...")
+
+
+n=int(input("Enter N:"))
+C={}
+for i in range (n):
+     B=Bank()
+     acno=B.openAccount()
+     C.setdefault(acno,B)
+
+print("SBI Main Manu") 
+print("1:Display All")   
+print("2:Search By Account No")  
+print("3:Deposit")   
+print("4:withdrawal")
+print("5:Exit")   
+ch=int(input ("Enter Choice:"))
+
+match ch:
+    case 1:
+        for customer in C.values():
+            customer.showAccount()         
+
+
+#========================================
+
+class Bank:
+    def openAccount(self):
+        self.__acno=input("Enter Account Number:")
+        self.__name=input("Enter name:")
+        self.__balance=int(input("Enter balance:"))
+        return self.__acno
+
+    def showAccount(self):
+        print(self.__acno,self.__name,self.__balance) 
+         
+    def Deposit(self):
+        amt=int(input("Enter Amount u want to Deposit:"))
+        self.__balance+=amt
+
+    def withdrawal(self):
+        amt=int(input("Enter Amount u want to withdraw:"))
+        if(self.__balance>=amt):
+           self.__balance-=amt
+
+        else:
+            print("Insufficiant Balance...")
+
+
+n=int(input("Enter N:"))
+C={}
+for i in range (n):
+     B=Bank()
+     acno=B.openAccount()
+     C.setdefault(acno,B)
+while(True):
+
+    print("SBI Main Manu") 
+    print("1:Display All")   
+    print("2:Search By Account No")  
+    print("3:Deposit")   
+    print("4:withdrawal")
+    print("5:Exit")   
+    ch=int(input ("Enter Choice:"))
+    match ch:
+
+        case 1:
+            for customer in C.values():
+                customer.showAccount()
+                
+        case 2:
+                acno=int(input("Enter Account Number u want to search"))  
+                customer=C.get(acno,"Customer Not Exits  ") 
+                if(isinstance(customer,Bank)):
+                    customer.showAccount()
+                else:
+                    print(customer)
+
+        case 3:
+                acno=int(input("Enter Account Number u want to search for Deposit"))  
+                customer=C.get(acno,"Customer Not Exits  ") 
+                if(isinstance(customer,Bank)):
+                    customer.showAccount()
+                    customer.Deposit
+                else:
+                    print(customer)
+
+
+        case 4:
+                acno=int(input("Enter Account Number u want to search for Whitdraw"))  
+                customer=C.get(acno,"Customer Not Exits  ") 
+                if(isinstance(customer,Bank)):
+                    customer.showAccount()
+                    customer.withdrawal
+                else:
+                    print(customer)
+
+        case 5:
+            print('Good by')
+        case 6:
+            print('wrong option')
+
+            if(ch==5):
+                break
+
+                
+        
+#========================================
